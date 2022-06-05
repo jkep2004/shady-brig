4ef7f62e34e9ce6e1ec5d8fd2bfe3e9c3816c9c3
jkep2004 <15320@cambornescience.co.uk>
2022-06-05 13:56:56 +0100

    Create level.js
    Contains the level BLOCK string

---
+++ level.js
@@ -0,0 +1,18 @@
+// BLOCK string for game level
+
+/* Stored in format ABABABAB
+    A is the number of this Tile
+    B is the type of Tile
+    / is endline, filling the rest of the row with air
+*/
+
+const levelSizeX = 100;
+const levelSizeY = 100;
+
+const LEVEL = ``;
+
+for (let y = 0; y < levelSizeY; x ++) {
+
+    LEVEL += `/`;
+
+}
