168bf67060eb1a0edd64067610f533d0eefa7aa9
jkep2004 <97905011+jkep2004@users.noreply.github.com>
2022-06-06 12:42:28 +0100

    Create dynamicTitle.js

---
+++ dynamicTitle.js
@@ -0,0 +1,15 @@
+let lines = 0;
+let read = 0;
+let length = 1;
+
+function add(n) {
+  lines += n;
+  read++;
+
+  if (read === length) {
+    document.title = `Shady Brig - ${lines}`;
+  }
+  
+}
+
+fetch("script.js").then(text => text.text()).then(e => add(e.split("\n").filter(x => x.trim().slice(0, 2) !== "//" && x.trim().length > 0 && x.match(/[a-zA-Z0-9]/gi)).length));
