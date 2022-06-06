461a57a0f983a1adc11d6cd37003be26873033df
jkep2004 <15320@cambornescience.co.uk>
2022-06-06 18:44:30 +0100

    Update game.js

--- game.js
+++ game.js
@@ -1,14 +1,16 @@
+var simRate, targetFPS, simSpeed; // Initialise global framerate variables
+
 function setup () {
 
     // Setup p5.js canvas object
 
     createCanvas(windowWidth, windowHeight);
 
-    // Initialise framerate variables
+    // Populate framerate variables
 
-    var simRate = 1; // Ensure movement is constant, is 1 at 60fps, 2 at 30fps and 0.5 at 120fps
-    const targetFPS = 60; // Game target framerate
-    const simSpeed = 1000 / targetFPS; // Game target time per frame (ms)
+    simRate = 1; // Ensure movement is constant, is 1 at 60fps, 2 at 30fps and 0.5 at 120fps
+    targetFPS = 60; // Game target framerate
+    simSpeed = 1000 / targetFPS; // Game target time per frame (ms)
     frameRate(targetFPS); // Set target framerate
 
 }
