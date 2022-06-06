53212ebb6cedaec7162641d0b4f4127ee06b93d8
jkep2004 <15320@cambornescience.co.uk>
2022-06-06 18:39:22 +0100

    Add in simulation speed accountability

--- game.js
+++ game.js
@@ -1,11 +1,21 @@
 function setup () {
 
+    // Setup p5.js canvas object
+
     createCanvas(windowWidth, windowHeight);
+
+    // Initialise framerate variables
+
+    var simRate = 1; // Ensure movement is constant, is 1 at 60fps, 2 at 30fps and 0.5 at 120fps
+    const targetFPS = 60; // Game target framerate
+    const simSpeed = 1000 / targetFPS; // Game target time per frame (ms)
+    frameRate(targetFPS); // Set target framerate
 
 }
 
 function draw () {
 
+    simRate = deltaTime / simSpeed; // Update simRate from current deltaTime
     
 
 }
