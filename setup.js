123bc52510dae93da977b870c44306445b9713f8
jkep2004 <15320@cambornescience.co.uk>
2022-06-06 20:23:16 +0100

    Add framerate handling

---
+++ setup.js
@@ -0,0 +1,20 @@
+var simRate, targetFPS, simSpeed; // Initialise global framerate variables
+
+function setup () {
+
+    // Setup p5.js canvas object
+
+    createCanvas(windowWidth, windowHeight);
+
+    // Populate framerate variables
+
+    simRate = 1; // Ensure movement is constant, is 1 at 60fps, 2 at 30fps and 0.5 at 120fps
+    targetFPS = 60; // Game target framerate
+    simSpeed = 1000 / targetFPS; // Game target time per frame (ms)
+    frameRate(targetFPS); // Set target framerate
+
+    // Start background music
+
+    audioHandler.playlist["backgroundMusic"].autoplay(true);
+
+}
