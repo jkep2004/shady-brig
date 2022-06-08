dcf67668f747eb3854b45bb27b3bf13243884198
jkep2004 <15320@cambornescience.co.uk>
2022-06-08 16:17:11 +0100

    Reset dynamicTitle to base

--- style/dynamicTitle.js
+++ style/dynamicTitle.js
@@ -2,30 +2,16 @@ let lines = 0;
 let read = 0;
 let length = 1;
 
-let total = 0;
-
 function lineCount (n) {
   lines += n;
   read++;
 
   if (read === length) {
 
-    total += lines;
+    document.title = `Shady Brig - ${total}`;
 
   }
   
 }
 
-// Sum all lines of code within code files
-
-fetch("../audio/audioHandler.js").then(text => text.text()).then(e => lineCount(e.split("\n").filter(x => x.trim().slice(0, 2) !== "//" && x.trim().length > 0 && x.match(/[a-zA-Z0-9]/gi)).length));
-fetch("../image/imageHandler.js").then(text => text.text()).then(e => lineCount(e.split("\n").filter(x => x.trim().slice(0, 2) !== "//" && x.trim().length > 0 && x.match(/[a-zA-Z0-9]/gi)).length));
 fetch("draw.js").then(text => text.text()).then(e => lineCount(e.split("\n").filter(x => x.trim().slice(0, 2) !== "//" && x.trim().length > 0 && x.match(/[a-zA-Z0-9]/gi)).length));
-fetch("entity.js").then(text => text.text()).then(e => lineCount(e.split("\n").filter(x => x.trim().slice(0, 2) !== "//" && x.trim().length > 0 && x.match(/[a-zA-Z0-9]/gi)).length));
-fetch("inputHandler.js").then(text => text.text()).then(e => lineCount(e.split("\n").filter(x => x.trim().slice(0, 2) !== "//" && x.trim().length > 0 && x.match(/[a-zA-Z0-9]/gi)).length));
-fetch("level.js").then(text => text.text()).then(e => lineCount(e.split("\n").filter(x => x.trim().slice(0, 2) !== "//" && x.trim().length > 0 && x.match(/[a-zA-Z0-9]/gi)).length));
-fetch("preload.js").then(text => text.text()).then(e => lineCount(e.split("\n").filter(x => x.trim().slice(0, 2) !== "//" && x.trim().length > 0 && x.match(/[a-zA-Z0-9]/gi)).length));
-fetch("setup.js").then(text => text.text()).then(e => lineCount(e.split("\n").filter(x => x.trim().slice(0, 2) !== "//" && x.trim().length > 0 && x.match(/[a-zA-Z0-9]/gi)).length));
-fetch("index.html").then(text => text.text()).then(e => lineCount(e.split("\n").filter(x => x.trim().slice(0, 2) !== "//" && x.trim().length > 0 && x.match(/[a-zA-Z0-9]/gi)).length));
-
-document.title = `Shady Brig - ${total}`;
