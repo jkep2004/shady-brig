3cbf90c593a9cd32b6405105ba22b597b1db6963
jkep2004 <15320@cambornescience.co.uk>
2022-07-07 18:08:01 +0100

    Add all files to dynamic title

--- style/dynamicTitle.js
+++ style/dynamicTitle.js
@@ -4,15 +4,25 @@ let length = 1;
 
 let files = [
 
-  "audioHandler.js",
-  "imageHandler.js",
-  "draw.js",
-  "entity.js",
-  "index.html",
-  "inputHandler.js",
-  "level.js",
-  "preload.js",
-  "setup.js"
+  'audioHandler.js',
+  'imageHandler.js',
+  'actuator.js',
+  'coin.js',
+  'collision.js',
+  'draw.js',
+  'edge.js',
+  'enemy.js',
+  'entity.js',
+  'index.html',
+  'inputHandler.js',
+  'ladder.js',
+  'level.js',
+  'player.js',
+  'preload.js',
+  'setup.js',
+  'surface.js',
+  'tile.js',
+  'weapon.js'
 
 ]
 
@@ -36,4 +46,4 @@ function lineCount (n) {
   
 }
 
-fetch("draw.js").then(text => text.text()).then(e => lineCount(e.split("\n").filter(x => x.trim().slice(0, 2) !== "//" && x.trim().length > 0 && x.match(/[a-zA-Z0-9]/gi)).length));
+fetch('draw.js').then(text => text.text()).then(e => lineCount(e.split('\n').filter(x => x.trim().slice(0, 2) !== '//' && x.trim().length > 0 && x.match(/[a-zA-Z0-9]/gi)).length));
