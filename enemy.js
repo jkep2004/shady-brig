b63bc3a139d40373efb00e6b898e19fa782df802
jkep2004 <15320@cambornescience.co.uk>
2022-07-07 20:07:59 +0100

    Make draw neater

--- enemy.js
+++ enemy.js
@@ -6,7 +6,7 @@ class Enemy extends Entity {
 
     static animationRate = 0.05;
 
-    constructor (posX, posY) {
+    constructor (posX, posY, surface) {
 
         super(posX, posY, Enemy.size.x, Enemy.size.y, Enemy.speed.x, Enemy.speed.y, Enemy.images['idle'][0]);
 
@@ -14,6 +14,8 @@ class Enemy extends Entity {
         this.direction = 1;
 
         this.mesh = new Mesh (this.pos.x, this.pos.y, this.size.x, this.size.y);
+
+        this.surface = surface;
 
         this.animationState = 0;
         this.animationRate = Enemy.animationRate;
@@ -24,53 +26,25 @@ class Enemy extends Entity {
 
         if (!this.show) return;
 
+        push();
+
+        scale(this.direction, 1);
+
         if (this.moving) {
 
-            push();
-
-            scale(this.direction, 1);
-
-            if (this.direction == 1) {
-
-                image(Enemy.images['run'][Math.floor(this.animationState)], this.pos.x, this.pos.y, this.size.x, this.size.y);
-
-            } else {
-
-                image(Enemy.images['run'][Math.floor(this.animationState)], - this.size.x - this.pos.x, this.pos.y, this.size.x, this.size.y);
-
-            }
+            image(Enemy.images['run'][Math.floor(this.animationState)], this.direction * this.pos.x, this.pos.y, this.direction * this.size.x, this.size.y);
 
             this.animationState = (this.animationState + (this.animationRate * 2 * simRate)) % 4;
 
-            pop();
-
         } else {
 
-            push();
-
-            scale(this.direction, 1);
-
-            if (this.direction == 1) {
-
-                image(Enemy.images['idle'][Math.floor(this.animationState)], this.pos.x, this.pos.y, this.size.x, this.size.y);
-
-            } else {
-
-                image(Enemy.images['idle'][Math.floor(this.animationState)], - this.size.x - this.pos.x, this.pos.y, this.size.x, this.size.y);
-
-            }
+            image(Enemy.images['idle'][Math.floor(this.animationState)], this.direction * this.pos.x, this.pos.y, this.direction * this.size.x, this.size.y);
 
             this.animationState = (this.animationState + (this.animationRate * simRate)) % 4;
 
-            pop();
-
         }
 
-    }
-
-    update () {
-
-        // pathfind to player
+        pop();
 
     }
 
